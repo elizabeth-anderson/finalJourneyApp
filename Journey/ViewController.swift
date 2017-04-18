182235062f7a487a19aa462a43f4a1ca7cec0ac3
elizabeth-anderson <student@elizabeths-computer.local>
2017-04-18 10:16:41 -0500

    Initial Commit

---
+++ Journey/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Journey
+//
+//  Created by Student on 4/17/17.
+//  Copyright © 2017 bhs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+ 
+
+
+}
+
