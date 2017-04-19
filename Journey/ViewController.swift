20dd2e424379ca9ea8311e0d69e1f43d8b2ea536
elizabeth-anderson <student@elizabeths-computer.local>
2017-04-19 10:22:54 -0500

    beth commit

--- Journey/ViewController.swift
+++ Journey/ViewController.swift
@@ -7,62 +7,62 @@
 //
 
 import UIKit
+//
+//class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+//{
+//    @IBOutlet weak var myTableView: UITableView!
+//    
+//    var myDiaryObject : [diaryClass] = []
+//
+//    override func viewDidLoad()
+//    {
+//        super.viewDidLoad()
+//        myDiaryObject.append(diaryClass(Location: "Florida", Date: "December 2016"))
+//         myDiaryObject.append(diaryClass(Location: "Indiana", Date: "February 2017"))
+//        
+//    }
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
-{
-    @IBOutlet weak var myTableView: UITableView!
-    
-    var myDiaryObject : [diaryClass] = []
-
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        myDiaryObject.append(diaryClass(Location: "Florida", Date: "December 2016"))
-         myDiaryObject.append(diaryClass(Location: "Indiana", Date: "February 2017"))
-        
-    }
-
-    @IBAction func addButtonTapped(_ sender: Any)
-    {
-    
-            let alert = UIAlertController(title: "add trip", message: nil, preferredStyle: .alert)
-            
-            alert.addTextField
-                {
-                    (collegeTextField) in collegeTextField.placeholder = "add college"
-            }
-            
-            alert.addTextField
-                {
-                    (locationTextField) in locationTextField.placeholder = "add location"
-            }
-            
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
-            alert.addAction(cancelAction)
-            
-            let addAction = UIAlertAction(title: "Add", style: .default)
-            {
-                (action) in
-                
-                let collegeTextField = alert.textFields?[0]
-                
-                let locationTextField = alert.textFields?[1]
-                
-                
-                self.myCollegeObject.append(collegeClass(Name: (collegeTextField?.text)!, Location: (locationTextField?.text)!))
-                
-                self.myTableView.reloadData()
-            }
-            
-            alert.addAction(addAction)
-            
-            present(alert, animated: true, completion: nil)
-            
-        }
+//    @IBAction func addButtonTapped(_ sender: Any)
+//    {
+//    
+//            let alert = UIAlertController(title: "add trip", message: nil, preferredStyle: .alert)
+//            
+//            alert.addTextField
+//                {
+//                    (collegeTextField) in collegeTextField.placeholder = "add college"
+//            }
+//            
+//            alert.addTextField
+//                {
+//                    (locationTextField) in locationTextField.placeholder = "add location"
+//            }
+//            
+//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+//            
+//            alert.addAction(cancelAction)
+//            
+//            let addAction = UIAlertAction(title: "Add", style: .default)
+//            {
+//                (action) in
+//                
+//                let collegeTextField = alert.textFields?[0]
+//                
+//                let locationTextField = alert.textFields?[1]
+//                
+//                
+//                self.myCollegeObject.append(collegeClass(Name: (collegeTextField?.text)!, Location: (locationTextField?.text)!))
+//                
+//                self.myTableView.reloadData()
+//            }
+//            
+//            alert.addAction(addAction)
+//            
+//            present(alert, animated: true, completion: nil)
+//            
+//        }
 
  
 
 
-}
+//}
 
