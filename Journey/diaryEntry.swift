24b343f6f429eeea8a22178c513759dc3c1ad278
elizabeth-anderson <student@elizabeths-computer.local>
2017-04-19 10:21:34 -0500

    beth commit

---
+++ Journey/diaryEntry.swift
@@ -0,0 +1,31 @@
+//
+//  diaryEntry.swift
+//  Journey
+//
+//  Created by Elizabeth on 4/19/17.
+//  Copyright © 2017 bhs. All rights reserved.
+//
+
+import UIKit
+
+class diaryEntry: UIViewController
+{
+    var location = ""
+    var date = ""
+    
+    init(Location:String, Date:String)
+    {
+       
+        location = Location
+        date = Date
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
---
+++ diaryClass.swift
@@ -0,0 +1,21 @@
+//
+//  diaryClass.swift
+//  Journey
+//
+//  Created by Elizabeth on 4/19/17.
+//  Copyright © 2017 bhs. All rights reserved.
+//
+
+import UIKit
+
+class diaryClass: NSObject {
+    var location = ""
+    var date = ""
+    
+    init(Location: String, Date: String)
+    {
+        location = location
+        date = Date
+    }
+
+}
